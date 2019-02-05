0808ba7d0e74fe6ef2ceb4e3d5355b4aac647361
Peter Law <PeterJCLaw@gmail.com>
2019-02-05 22:04:00 +0000

    Allow PyYAML 4
    PyYAML 4 appears to be compatible for our usages and this shuts up
    the errant GitHub security warning about it.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 install_requires = [
-    'PyYAML >=3.11, <4',
+    'PyYAML >=3.11, <5',
     'sr.comp.ranker >=1.3, <2',
     'python-dateutil >=2.2, <3'
 ]
@@ -17,7 +17,7 @@ if sys.version_info < (3, 4):
 
 setup(
     name='sr.comp',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     namespace_packages=['sr', 'sr.comp'],
     description='Student Robotics Competition Software',
