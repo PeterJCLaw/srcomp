05b89b3c178796dbe082c9278b7a63a3636d541c
Peter Law <PeterJCLaw@gmail.com>
2020-03-10 23:52:35 +0000

    Make these exports explicit

--- sr/comp/knockout_scheduler/__init__.py
+++ sr/comp/knockout_scheduler/__init__.py
@@ -3,3 +3,9 @@
 from .base_scheduler import UNKNOWABLE_TEAM
 from .automatic_scheduler import KnockoutScheduler
 from .static_scheduler import StaticScheduler
+
+__all__ = (
+    'KnockoutScheduler',
+    'StaticScheduler',
+    'UNKNOWABLE_TEAM',
+)
