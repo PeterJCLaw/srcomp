80c35d266aefb0ba64c45d66582bb9f5d4b561b0
Peter Law <PeterJCLaw@gmail.com>
2020-03-11 19:13:01 +0000

    Call it 1.2 now we're Python 3 only and using league-ranker
    No, this probably isn't SemVer.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='sr.comp',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(exclude=('tests',)),
     namespace_packages=['sr', 'sr.comp'],
     description="Student Robotics Competition Software",
