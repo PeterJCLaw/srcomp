c33ed989447ad4702006c86681f4e78371af4d92
Peter Law <PeterJCLaw@gmail.com>
2020-04-11 18:24:44 +0100

    Reorder classes to avoid forward references

--- sr/comp/match_period.py
+++ sr/comp/match_period.py
@@ -4,6 +4,25 @@ from collections import namedtuple
 from enum import Enum, unique
 
 
+@unique
+class MatchType(Enum):
+    league = 'league'
+    knockout = 'knockout'
+    tiebreaker = 'tiebreaker'
+
+
+Match = namedtuple('Match', [
+    'num',
+    'display_name',
+    'arena',
+    'teams',
+    'start_time',
+    'end_time',
+    'type',
+    'use_resolved_ranking',
+])
+
+
 class MatchPeriod(namedtuple('MatchPeriod', [
     'start_time',
     'end_time',
@@ -21,22 +40,3 @@ class MatchPeriod(namedtuple('MatchPeriod', [
             self.start_time.strftime('%H:%M'),
             self.end_time.strftime('%H:%M'),
         )
-
-
-Match = namedtuple('Match', [
-    'num',
-    'display_name',
-    'arena',
-    'teams',
-    'start_time',
-    'end_time',
-    'type',
-    'use_resolved_ranking',
-])
-
-
-@unique
-class MatchType(Enum):
-    league = 'league'
-    knockout = 'knockout'
-    tiebreaker = 'tiebreaker'
