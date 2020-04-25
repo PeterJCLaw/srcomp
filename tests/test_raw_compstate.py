fe8680c219a1e587dd924d3a7ed3061204e89abd
Peter Law <PeterJCLaw@gmail.com>
2020-04-25 17:39:59 +0100

    Annotate these tests

--- tests/test_raw_compstate.py
+++ tests/test_raw_compstate.py
@@ -1,28 +1,33 @@
+import datetime
 import os.path
 import subprocess
 import unittest
+from typing import List, Optional
+
+from dateutil.tz import UTC
 
 from sr.comp.comp import SRComp
 from sr.comp.match_period import Match, MatchType
 from sr.comp.raw_compstate import RawCompstate
+from sr.comp.types import ArenaName, MatchNumber, TLA
 
 DUMMY_PATH = os.path.dirname(os.path.abspath(__file__)) + '/dummy'
 
 
 def build_match(
-    num,
-    arena,
-    teams=None,
-    start_time=None,
-    end_time=None,
-    type_=None,
-    use_resolved_ranking=False,
-):
+    num: int,
+    arena: str,
+    teams: Optional[List[Optional[TLA]]] = None,
+    start_time: datetime.datetime = datetime.datetime(2020, 1, 25, 11, 0, tzinfo=UTC),
+    end_time: datetime.datetime = datetime.datetime(2020, 1, 25, 11, 5, tzinfo=UTC),
+    type_: MatchType = MatchType.league,
+    use_resolved_ranking: bool = False,
+) -> Match:
     return Match(
-        num,
+        MatchNumber(num),
         "Match {n}".format(n=num),
-        arena,
-        teams,
+        ArenaName(arena),
+        teams or [],
         start_time,
         end_time,
         type_,
@@ -31,12 +36,12 @@ def build_match(
 
 
 class RawCompstateTests(unittest.TestCase):
-    def test_load(self):
+    def test_load(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
         comp = state.load()
         self.assertIsInstance(comp, SRComp)
 
-    def test_get_score_path(self):
+    def test_get_score_path(self) -> None:
         m = build_match(0, 'A', type_=MatchType.league)
         state = RawCompstate(DUMMY_PATH, local_only=True)
         path = state.get_score_path(m)
@@ -45,7 +50,7 @@ class RawCompstateTests(unittest.TestCase):
             "Path expected to exist within dummy state",
         )
 
-    def test_load_score(self):
+    def test_load_score(self) -> None:
         m = build_match(0, 'A', type_=MatchType.league)
         state = RawCompstate(DUMMY_PATH, local_only=True)
         score = state.load_score(m)
@@ -57,7 +62,7 @@ class RawCompstateTests(unittest.TestCase):
         expected = ['CLY', 'TTN']
         self.assertEqual(expected, teams, score)
 
-    def test_load_shepherds(self):
+    def test_load_shepherds(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
         shepherds = state.load_shepherds()
 
@@ -84,35 +89,35 @@ class RawCompstateTests(unittest.TestCase):
 
         self.assertEqual(expected, shepherds, "Wrong shepherds data loaded")
 
-    def test_shepherding(self):
+    def test_shepherding(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
         self.assertTrue(state.shepherding)
 
-    def test_layout(self):
+    def test_layout(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
         self.assertTrue(state.layout)
 
-    def test_contains_HEAD(self):
+    def test_contains_HEAD(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
 
         has_HEAD = state.has_commit('HEAD')
         self.assertTrue(has_HEAD, "Should have HEAD commit!")
 
-    def test_git_return_output(self):
+    def test_git_return_output(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
 
         output = state.git(['show'], return_output=True)
 
         self.assertTrue(output.startswith('commit '), output)
 
-    def test_git_no_return_output(self):
+    def test_git_no_return_output(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
 
         output = state.git(['rev-parse', 'HEAD'])
 
         self.assertEqual(0, output, "Should succeed and return exit code")
 
-    def test_git_return_output_when_error(self):
+    def test_git_return_output_when_error(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
 
         with self.assertRaises(
@@ -122,7 +127,7 @@ class RawCompstateTests(unittest.TestCase):
             output = state.git(['this-is-not-a-valid-command'], return_output=True)
             print(output)
 
-    def test_git_when_error(self):
+    def test_git_when_error(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
 
         with self.assertRaises(
@@ -132,7 +137,7 @@ class RawCompstateTests(unittest.TestCase):
             output = state.git(['this-is-not-a-valid-command'])
             print(output)
 
-    def test_git_converts_error(self):
+    def test_git_converts_error(self) -> None:
         state = RawCompstate(DUMMY_PATH, local_only=True)
         error_msg = "Our message that something went wrong"
 
