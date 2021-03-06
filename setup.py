52388170b33862e4cf7654da2db5f36d526e80b5
Peter Law <PeterJCLaw@gmail.com>
2021-03-06 23:11:43 +0000

    Patch version for small setup.py fixes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('setup-requirements.txt') as f:
 
 setup(
     name='sr.comp',
-    version='1.4.3',
+    version='1.4.4',
     url='https://github.com/PeterJCLaw/srcomp/wiki',
     project_urls={
         'Documentation': 'https://srcomp.readthedocs.org/',
