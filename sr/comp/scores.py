9d010b523216d5bb669930d0459c1fa6107d32b4
Peter Law <PeterJCLaw@gmail.com>
2024-03-04 21:14:03 +0000

    Improve diagnostic information about bad score files

--- sr/comp/scores.py
+++ sr/comp/scores.py
@@ -246,7 +246,13 @@ class BaseScores:
         if match_id in self.game_points:
             raise DuplicateScoresheet(match_id)
 
-        game_points = get_validated_scores(self._scorer, score_data)
+        try:
+            game_points = get_validated_scores(self._scorer, score_data)
+        except Exception as e:
+            raise RuntimeError(
+                "Error processing score for match {}{}".format(*match_id),
+            ) from e
+
         self.game_points[match_id] = game_points
 
         # Build the disqualification dict
